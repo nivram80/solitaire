7461e3c42436563547d9ad68d995de6763c96d89
Todd Nichols <nivram80@gmail.com>
2019-11-16 16:46:51 -0600

    move foundations into its own component

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import { deck } from './utilities/deck';
+  import Foundations from './Foundations.svelte';
   import Tableau from './Tableau.svelte';
   import Card from './Card.svelte';
 
@@ -136,13 +137,7 @@
 </header>
 
 <main class="main">
-  <section class="foundations">
-    <div class="foundation diamond-foundation">A</div>
-    <div class="foundation heart-foundation">A</div>
-    <div class="foundation club-foundation">A</div>
-    <div class="foundation spade-foundation">A</div>
-  </section>
-
+  <Foundations />
   <Tableau {tableau} on:updatePile={onUpdatePile} />
 </main>
 
@@ -200,27 +195,5 @@
     margin: 0;
     padding: 4px 8px;
     cursor: pointer;
-  }
-
-  .foundations {
-    grid-area: foundation;
-    display: grid;
-    flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    padding: 8px 0;
-  }
-
-  .foundation {
-    margin-bottom: 12px;
-    box-sizing: border-box;
-    width: 80px;
-    height: 120px;
-    border-radius: 4px;
-    border: 4px solid rgba(0, 0, 0, 0.1);
-    padding: 4px 6px;
-    font-size: 80px;
-    color: rgba(0, 0, 0, 0.1);
-    text-align: center;
   }
 </style>
---
+++ src/Foundations.svelte
@@ -0,0 +1,30 @@
+<section class="foundations">
+  <div class="foundation diamond-foundation">A</div>
+  <div class="foundation heart-foundation">A</div>
+  <div class="foundation club-foundation">A</div>
+  <div class="foundation spade-foundation">A</div>
+</section>
+
+<style>
+  .foundations {
+    grid-area: foundation;
+    display: grid;
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    padding: 8px 0;
+  }
+
+  .foundation {
+    margin-bottom: 12px;
+    box-sizing: border-box;
+    width: 80px;
+    height: 120px;
+    border-radius: 4px;
+    border: 4px solid rgba(0, 0, 0, 0.1);
+    padding: 4px 6px;
+    font-size: 80px;
+    color: rgba(0, 0, 0, 0.1);
+    text-align: center;
+  }
+</style>
